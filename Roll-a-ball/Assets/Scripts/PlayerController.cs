6a46705d06377c0f6e1428aca324574db63c6fc3
MladenLM <mladenlamesevic@gmail.com>
2021-08-24 10:34:49 +0200

    game-finished
    added prefabs, UI, and score

--- Roll-a-ball/Assets/Scripts/PlayerController.cs
+++ Roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -2,30 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
+
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
+    public TextMeshProUGUI countText;
     private Rigidbody rb;
-    private float movmentX;
-    private float movmentY;
-    
+    private int count;
+    private float movementX;
+    private float movementY;
+    public GameObject winTextObject;
+
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();    
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        setCountText();
     }
 
-    void OnMove(InputValue movmentValue)
+    private void OnMove(InputValue movementValue)
     {
-        Vector2 movmentVector = movmentValue.Get<Vector2>();
+        Vector2 movementVector = movementValue.Get<Vector2>();
 
-        movmentX = movmentVector.x;
-        movmentY = movmentVector.y;
+        movementX = movementVector.x;
+        movementY = movementVector.y;
     }
 
-    void FixedUpdate()
+    void setCountText()
     {
-        Vector3 movment = new Vector3(movmentX, 0.0f, movmentY);
-        rb.AddForce(movment * speed);
+        countText.text = "Count: " + count.ToString();
+        if (count == 5)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+        }
     }
 }
